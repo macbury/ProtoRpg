5f5957ff80320ffa8f601c7dd0e81f19ba7d6c51
Arkadiusz Buras <macbury@gmail.com>
2016-07-19 14:46:17 +0200

    Added unit testing

---
+++ Test/Core/Utils/TestXmlManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test {
+  [TestFixture()]
+  public class TestXmlManager {
+
+    [Test()]
+    public void ItShouldLoadDummyObjectFromXml() {
+      Assert.AreEqual(true, true);
+    }
+  }
+}
+
