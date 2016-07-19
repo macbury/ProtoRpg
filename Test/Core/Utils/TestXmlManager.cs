1040c2deb382ddd3244f82b0f7e3af0478efb30d
Arkadiusz Buras <macbury@gmail.com>
2016-07-19 16:51:49 +0200

    Tests for xml manager

--- Test/Core/Utils/TestXmlManager.cs
+++ Test/Core/Utils/TestXmlManager.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using NUnit.Framework;
+using ProtoRpg;
 
 namespace Test {
+
+  public class DummyXmlObject {
+    public int ValueToLoad;
+  }
+
   [TestFixture()]
   public class TestXmlManager {
 
     [Test()]
     public void ItShouldLoadDummyObjectFromXml() {
-      Assert.AreEqual(true, true);
+      XmlManager<DummyXmlObject> xml = new XmlManager<DummyXmlObject>();
+      DummyXmlObject dummy = xml.Load("Fixtures/dummy_xml_object.xml");
+      Assert.IsNotNull(dummy);
+      Assert.AreEqual(dummy.ValueToLoad, 10);
     }
   }
 }
